4bdc5ccc96949e05ac6f21d437ca4f53e18f493a
RickuBoi <ricky.knc15@gmail.com>
2023-04-10 03:09:26 +0700

    added REST Api

---
+++ database/factories/InvoiceFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Invoice;
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Invoice>
+ */
+class InvoiceFactory extends Factory
+{
+
+    /**
+     * The name of the factory's corresponding model.
+     * 
+     * @var string
+     */
+    protected $model = Invoice::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $status = $this->faker->randomElement(['B', 'P', 'V']); //B -> Billed, P -> Paid, V -> Void
+
+        return [
+            'customer_id' => Customer::factory(),
+            'amount' => $this->faker->numberBetween(100, 20000),
+            'status' => $status,
+            'billed_date' => $this->faker->dateTimeThisDecade(),
+            'paid_date' => $status == 'P' ? $this->faker->dateTimeThisDecade() : NULL
+        ];
+    }
+}
